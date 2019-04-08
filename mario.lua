6e0786775433e2c47b3bc6292ad4e7c55324f929
Shiqing <shiqing-thu18@yandex.com>
2019-04-08 20:57:56 +0800

    :sparkles: Implement round init screen

--- mario.lua
+++ mario.lua
@@ -3,22 +3,68 @@
 -- desc:   Time to clear your Steam wishlist
 -- script: lua
 
-t=0
-x=96
-y=24
+W=240
+H=136
+
+n_rounds=4
+cur_round=0
+
+-- 0: round init
+-- 1: running
+-- 2: lottery
+-- Others TODO
+cur_scene=0
+-- Time of entering current scene
+scene_start=0
+
+ROUND_INIT_DUR=2000
+
+function change_scene(id)
+	cur_scene=id
+	scene_start=time()
+end
+
+function spr_c(id,x,y,w,h,keyc)
+	spr(id,x-w*4,y-h*4,keyc or -1,
+	    1,0,0,w,h)
+end
+
+function print_c(text,x,y,colour,scale)
+	scale=scale or 1
+	local w=print(text,999,999,colour,false,scale,true)
+	print(text,x-w//2,y-(6*scale)//2,colour,false,scale,true)
+end
+
+function round_init_screen(t)
+	cls(0)
+	spr_c(0,W*0.382,H*0.5,2,2)
+	print_c('x',W/2,H/2,12,2)
+	print_c(tostring(n_rounds-cur_round),W*0.618,H/2,12,2)
+
+	if t>=ROUND_INIT_DUR then
+		change_scene(1)
+	end
+end
+
+function running_screen(t)
+	cls(8)
+end
 
 function TIC()
-
-	if btn(0) then y=y-1 end
-	if btn(1) then y=y+1 end
-	if btn(2) then x=x-1 end
-	if btn(3) then x=x+1 end
-
-	cls(13)
-	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
-	print("HELLO WORLD!",84,84)
-	t=t+1
+	local t=time()-scene_start
+	if cur_scene == 0 then
+		round_init_screen(t)
+	elseif cur_scene == 1 then
+		running_screen(t)
+	end
 end
+
+-- <TILES>
+-- 000:0ffffffffdccccccfdddddddfdddddddfdddfdddfdddfdddfdddfdddfdddfddd
+-- 001:fffffff0ccccccdfdddddddfdddddddfdfdddddfdfdddddfdfdddddfdfdddddf
+-- 016:fdddddddfdddddddfdddddddfeeeeeeefeeeeeeefeeeeeeefeeeeeee0fffffff
+-- 017:dddddddfdddddddfdddddddfeeeeeeefeeeeeeefeeeeeeefeeeeeeeffffffff0
+-- </TILES>
 
 -- <WAVES>
 -- 000:00000000ffffffff00000000ffffffff
